9ead7a868267f5257ce28d879ed5876250da7576
Jens Giehl <mobil.jgiehl@gmail.com>
2019-02-01 22:39:33 +0100

    Try to set status

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-new Vue({
+var app = new Vue({
     el: '#app',
     data() {
         return {
-            items: null
+            bggUser: null,
+            items: null,
+            title: null
+        }
+    },
+    methods: {
+        bggUserSearch: function (event) {
+            for (let i=0; i < this.items.length; i++) {
+                let item = this.items[i];
+                if(item.expansions) {
+                    for (let j=0; j < item.expansions.length; j++) {
+                        let subItem = item.expansions[j];
+                        subItem.show=false
+                    } 
+                }
+            }
         }
     },
     mounted() {
         axios
-            .get("https://api.myjson.com/bins/14th1k")
-            .then(response => (this.items = response.data.games))
+            .get("https://api.myjson.com/bins/1h7ctg")
+            .then(response => {this.items = response.data.games; this.title=response.data.title})
     }
 })
